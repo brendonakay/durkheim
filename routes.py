cc3505c053bd8aa7fd6533e6a55f58e796101a82
Brendon Kay <bak04280@gmail.com>
2017-01-15 23:15:30 -0500

    added basic nav

--- routes.py
+++ routes.py
@@ -3,10 +3,24 @@ from flask import render_template
 
 app = Flask(__name__)
 
+navigation = [
+    {
+    'href': '/',
+    'caption': 'home'
+    },
+
+    {
+    'href': '/page',
+    'caption': 'page'
+    }
+]
+
+
 @app.route('/')
 def hello(name=None):
-    return render_template('index.html', name=name)
+    return render_template('index.html', navigation=navigation)
 
-# @app.route('/')
-# def index():
-#     return 'Durkheim project.' 
+
+@app.route('/page')
+def hello(name=None):
+    return render_template('page.html', navigation=navigation)
